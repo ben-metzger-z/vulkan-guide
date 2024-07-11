2ceb3a9a2aa358b27ef2cd28c68b6d88426aef06
Ben Metzger <ben.metzger@zethon.com>
2024-07-11 16:53:26 +0100

    Vulkan Shaders - Code

--- src/vk_descriptors.cpp
+++ src/vk_descriptors.cpp
@@ -1,1 +1,31 @@
 ﻿#include <vk_descriptors.h>
+
+void DescriptorLayoutBuilder::add_binding(uint32_t binding, VkDescriptorType type) {
+    VkDescriptorSetLayoutBinding new_bind{};
+    new_bind.binding = binding;
+    new_bind.descriptorCount = 1;
+    new_bind.descriptorType = type;
+
+    bindings.push_back(new_bind);
+}
+
+void DescriptorLayoutBuilder::clear() {
+    bindings.clear();
+}
+
+VkDescriptorSetLayout DescriptorLayoutBuilder::build(VkDevice device, VkShaderStageFlags shader_stage_flags,
+                                                     void *pNext, VkDescriptorSetLayoutCreateFlags) {
+    for (auto & binding : bindings) {
+        binding.stageFlags |= shader_stage_flags;
+    }
+
+    VkDescriptorSetLayoutCreateInfo info = {.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO};
+    info.pNext = pNext;
+
+    info.pBindings = bindings.data();
+    info.bindingCount = static_cast<uint32_t>(bindings.size());
+    info.flags = flags;
+
+    VkDescriptorSetLayout set;
+    VK_CHECK(vkCreateDescriptorSetLayout(device, &info, nullptr, &set));
+}
